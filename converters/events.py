67c3b486d449cdecf0198b9f6252acfb6794ab82
briggySmalls <wasso14@hotmail.com>
2018-09-11 08:08:29 +0100

    Set facebook event info on events

--- converters/events.py
+++ converters/events.py
@@ -2,6 +2,40 @@ import wpalchemy.classes as wp
 from sqlalchemy import and_
 
 from converters.converter import Converter
+from converters.helpers import create_post_meta
+
+
+# Facebook events to attach to event posts
+FACEBOOK_EVENTS = {
+    2717: 408977832915985,
+    2838: 408977832915985,
+    2480: 1991989291022482,
+    2840: 1991989291022482,
+    2059: 1519186918200845,
+    2877: 1519186918200845,
+    1723: 1245388985568706,
+    2878: 1245388985568706,
+    1998: 219818301849805,
+    2879: 219818301849805,
+    1670: 742875849213338,
+    2880: 742875849213338,
+    1625: 1241648545915838,
+    2881: 1241648545915838,
+    1523: 693827704114924,
+    2882: 693827704114924,
+    1472: 693827704114924,
+    2883: 693827704114924,
+    1371: 323297304705639,
+    2884: 323297304705639,
+    1287: 323297304705639,
+    2885: 323297304705639,
+    1065: 302739840069635,
+    2886: 302739840069635,
+    832: 582355721933792,
+    2887: 582355721933792,
+    2890: 457764397724406,
+    2891: 582355721933792,
+}
 
 
 class EventsConverter(Converter):
@@ -20,6 +54,13 @@ class EventsConverter(Converter):
         event_posts.update(
             {wp.Post.post_type: 'sd-event'},
             synchronize_session='fetch')
+
+        for event in event_posts:
+            if event.ID in FACEBOOK_EVENTS:
+                create_post_meta(
+                    self.source.session,
+                    event.ID,
+                    {'sd_event_facebook_event': FACEBOOK_EVENTS[event.ID]})
 
         # Remove the category term
         self.source.session.delete(events_category)
